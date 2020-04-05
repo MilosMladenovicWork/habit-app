0216e40d359a508e5adcd5bd546e28e505266a5d
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-04-05 16:11:25 +0200

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Swiper from 'react-id-swiper'
+import {Route, withRouter} from 'react-router-dom'
 import 'swiper/css/swiper.css'
 
 import Header from './components/header'
@@ -17,11 +18,37 @@ const params = {
   }
 }
 
-function App() {
+function App({history}) {
+
+  const [name, setName] = useState()
+
+  function handleClick(e){
+    e.preventDefault()
+    localStorage.setItem('username', name)
+    history.push('/home')
+  }
+
+  function handleChange(e){
+    setName(e.target.value)
+  }
+
+  function resetStorage(e){
+    e.preventDefault()
+    localStorage.clear()
+    history.push('/')
+  }
+
+  useEffect(() => {
+    if(localStorage.getItem('username') !== null){
+      history.push('/home')
+    }
+  }, [])
+
   return (
     <div className="App">
       <Header/>
-      <Swiper {...params}>
+      <Route path='/' exact render={() => 
+        <Swiper {...params}>
         <div className='introduction-page'>
           <OutShadowContainer>
             <img src={welcomeImg} alt='Welcome' className='introduction-image'/>
@@ -53,13 +80,21 @@ function App() {
             <img src={welcomeImg} alt='Welcome' className='introduction-image'/>
           </OutShadowContainer>
           <form action='#' method='POST'>   
-            <input type='text' name='name' placeholder='Name' className='input-field'/>
-            <button type='submit' className='submit-button'>Confirm</button>
+            <input type='text' name='name' placeholder='Name' className='input-field' onChange={e => handleChange(e)}/>
+            <button type='submit' className='submit-button' onClick={e => handleClick(e)}>Confirm</button>
           </form>
         </div>
       </Swiper>
+      }/>
+      <Route path='/home' render={() => 
+        <div>
+          Hello logged user
+          <button type='submit' className='submit-button' onClick={e => resetStorage(e)}>Reset</button>
+        </div>
+      }/>
+      
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
