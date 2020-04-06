30efeb9997fbbbad6527bbc7209cb7cc54fb8cbc
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-04-06 19:31:57 +0200

    added check mechanism and animations

--- src/components/habits.js
+++ src/components/habits.js
@@ -1,12 +1,18 @@
 import React, {useState} from 'react'
 
 import OutShadowContainer from './outshadowcontainer'
+import CheckBox from './checkbox'
 import testImg from '../images/logo.svg'
 import testIcon from '../images/icon.svg'
 
 const habitsArray = [
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Sample text her',
+    icon:testIcon,
+    completed:false
+  },
+  {
+    title:'Sample text here. Sample text here',
     icon:testIcon,
     completed:false
   },
@@ -16,52 +22,47 @@ const habitsArray = [
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Sample text heaaaaaaaaaaaaa aaaaaaaaaaaaa a',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here. aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa a',
+    title:'Sample text here. ',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Sample tet here.',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Sample here.',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Samt here.',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Sample tt here.',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Sple text here.',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Samle text here.',
     icon:testIcon,
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
-    icon:testIcon,
-    completed:false
-  },
-  {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. ample text here.',
     icon:testIcon,
     completed:false
   }
@@ -71,16 +72,31 @@ function Habits(){
 
   const [habits, setHabits] = useState(habitsArray)
 
+  function handleClick(e){
+    let habitTitle = e.target.parentNode.parentNode.childNodes[1].innerHTML
+    setHabits((prevState) => {
+      let newArray = [...prevState]
+      newArray.forEach(habit => {
+        if(habit.title === habitTitle){
+          habit.completed = !habit.completed 
+        }
+      })
+      return newArray
+    })
+  }
+
   return(
     <div>
       {habits.map((habit, index) => 
-        <OutShadowContainer key={index} style={{marginBottom:'20px'}}>
+        <OutShadowContainer key={index} style={{marginBottom:'20px', transform:habit.completed ? 'scale(0.85)' : 'scale(1)', opacity:habit.completed ? '0.6': '1'}}>
           <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
             <img className='icon' src={habit.icon} alt='icon' style={{height:'27px'}}/>
             <div className='title'>
               {habit.title}
             </div>
-            <img className='checkbox' src={habit.completed ? null : testImg} alt='checkbox' style={{height:'27px'}}/>
+            <div className='checkbox' onClick={(e) => handleClick(e)} alt='checkbox' style={{height:'27px'}}>
+              <CheckBox completed={habit.completed}/>
+            </div>
           </div>
         </OutShadowContainer>)}
     </div>
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 
 import OutShadowContainer from './outshadowcontainer'
+import CheckBox from './checkbox'
 import testImg from '../images/logo.svg'
 import testIcon from '../images/icon.svg'
 
@@ -17,7 +18,7 @@ const tasksArray = [
     completed:false
   },
   {
-    title:'Sample text here. Sample text here.',
+    title:'Sample text here. Sample text here. aaaaa',
     icon:testIcon,
     completed:false
   }
@@ -28,26 +29,30 @@ function Tasks(){
   const [tasks, setTasks] = useState(tasksArray)
 
   function handleClick(e){
-    let taskTitle = e.target.parentNode.childNodes[1].innerHTML
-    let filteredTasks = tasks.filter(task => task.title !== taskTitle)
-    let clickedTask = tasks.filter(task => task.title === taskTitle)
-    clickedTask.completed = !clickedTask.completed
-    setTasks([
-      ...filteredTasks,
-      ...clickedTask
-    ])
+    let taskTitle = e.target.parentNode.parentNode.childNodes[1].innerHTML
+    setTasks((prevState) => {
+      let newArray = [...prevState]
+      newArray.forEach(task => {
+        if(task.title === taskTitle){
+          task.completed = !task.completed 
+        }
+      })
+      return newArray
+    })
   }
 
   return(
     <div>
       {tasks.map((task, index) => 
-        <OutShadowContainer key={index} style={{marginBottom:'20px'}}>
+        <OutShadowContainer key={index} style={{marginBottom:'20px', transform:task.completed ? 'scale(0.85)' : 'scale(1)', opacity:task.completed ? '0.6': '1'}}>
           <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
             <img className='icon' src={task.icon} alt='icon' style={{height:'27px'}}/>
             <div className='title'>
               {task.title}
             </div>
-            <img onClick={(e) => handleClick(e)} className='checkbox' src={task.completed ? null : testImg} alt='checkbox' style={{height:'27px'}}/>
+            <div className='checkbox' onClick={(e) => handleClick(e)} alt='checkbox'>
+              <CheckBox completed={task.completed}/>
+            </div>
           </div>
         </OutShadowContainer>)}
     </div>
