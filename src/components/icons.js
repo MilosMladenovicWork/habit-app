cb7fbac07433dd36849468de0a1a60fcd52f7f1d
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-04-09 14:43:04 +0200

    fixed icons layout

--- src/components/icons.js
+++ src/components/icons.js
@@ -8,6 +8,8 @@ import megalophone from '../images/megalophone.svg'
 import earth from '../images/earth.svg'
 import news from '../images/news.svg'
 
+import './icons.css'
+
 const icons = [
   card,
   mail,
@@ -22,12 +24,12 @@ function Icons({
   selectedIcon
 }){
   return(
-    <div style={{display:'flex'}}>
+    <div className='icons-container'>
       {
         icons.map((icon, index) =>
-          <div style={{width:'fit-content'}} onClick={(e) => handleIcon(e)}>
-            <OutShadowContainer className={selectedIcon === icon ? 'in-shadow-container' : null} key={index}>
-              <img src={icon} style={{width:'27px'}}/>
+          <div style={{width:'fit-content', width:'21%', marginLeft:'2%', marginRight:'2%'}} onClick={(e) => handleIcon(e)}>
+            <OutShadowContainer style={{marginBottom:'12px'}} className={selectedIcon === icon ? 'in-shadow-container' : null} key={index}>
+              <img src={icon} style={{width:'100%'}}/>
             </OutShadowContainer>
           </div>
         ) 
